be94ba2e40d7a68acb816679294129922cc5c9ea
natalia zhydejkina <Karina.Varlena@instantconnectnow.com>
2020-09-12 22:45:06 +0300

    lesson1

---
+++ lesson1/src/com/company/Main.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        float f = 3.14f;
+        int i = 1;
+        int i2;
+        i2 = 3;
+        char c = 'c';
+        long l = 1234567890l;
+        byte b = 126;
+        boolean boo = true;
+        short s = 56;
+        String st = "jnasc";
+
+        int sum = i + i2;
+        int diff = i - i2;
+        int umn = i2 * i;
+        double del = i / i2;
+        int birthday = 2006+9+3;
+        System.out.println(sum);
+        System.out.println(diff);
+        System.out.println(umn);
+        System.out.println(del);
+        System.out.println(birthday);
+
+
+    }
+}
